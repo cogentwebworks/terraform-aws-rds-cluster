ea5fc078a1a422dcdf13f1aac882918ceec451f2
Pichate Ins <dearteno@gmail.com>
2020-08-11 13:26:00 +0700

    build(tf): upgrade to provider version

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.1"
+      vversion = ">= 3.1"
     }
     null = {
       source  = "hashicorp/null"
